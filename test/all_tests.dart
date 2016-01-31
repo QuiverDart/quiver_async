1dcf7bf700bfc65452c42c6464ef4854c6a6eb09
Alec Henninger <ahenning@redhat.com>
2016-01-31 08:35:59 -0500

    Move fake_async_test to follow existing conventions

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -14,20 +14,20 @@
 
 import 'countdown_timer_test.dart' as countdown_timer_test;
 import 'create_timer_test.dart' as create_timer_test;
-import 'fake_async_test.dart' as fake_async_test;
 import 'future_group_test.dart' as future_group_test;
 import 'future_stream_test.dart' as future_stream_test;
 import 'iteration_test.dart' as iteration_test;
 import 'metronome_test.dart' as metronome_test;
 import 'stream_router_test.dart' as stream_router_test;
+import 'testing/all_tests.dart' as testing;
 
 main() {
   countdown_timer_test.main();
   create_timer_test.main();
-  fake_async_test.main();
   future_group_test.main();
   future_stream_test.main();
   iteration_test.main();
   metronome_test.main();
   stream_router_test.main();
+  testing.main();
 }
